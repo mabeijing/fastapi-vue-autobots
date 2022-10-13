e26b754af76921033a5d2e7c0920df80aa69a09e
mabeijing <58149278@qq.com>
2022-10-13 15:14:31 +0800

    add startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
-pwd = `pwd`
+#!/bin/bash
 
-echo $pwd
+source /home/ubuntu/virtualenv/fastapi-vue-autobots/bin/activate
+
+cd /home/ubuntu/projects/fastapi-vue-autobots
+
+uvicorn app.main:app --reload
