658ec921d29ddc628445c63497905ccf3b403144
BeijingMa <beijingm@vmware.com>
2022-10-25 18:01:32 +0800

    add

--- app/admin/schema.py
+++ app/admin/schema.py
@@ -1,1 +1,68 @@
-# 路由文件配置
+from typing import List
+
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+# SQLAlchemy specific code, as with any other app
+DATABASE_URL = "sqlite:///./test.db"
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+
+class Note(BaseModel):
+    id: int = Field(default=1, exclude=True, repr=False)
+    text: str
+    completed: bool
+
+    class Config:
+        orm_mode = True
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: Note):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return note
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('schema:app')
--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /home/ubuntu/virtualenv/fastapi-vue-autobots/bin/activate
+source /home/ubuntu/virtualenv/fastapi-vue-autobots-3.11.0/bin/activate
 
-cd /home/ubuntu/projects/fastapi-vue-autobots
+cd /home/ubuntu/projects/fastapi-vue-autobots || return
 
 uvicorn app.main:app --reload
