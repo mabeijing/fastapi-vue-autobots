380e56df317bb3328f6858bbdf51fd99029568a5
BeijingMa <beijingm@vmware.com>
2022-11-03 15:14:00 +0800

    update async curd

--- app/module_route/access.py
+++ app/module_route/access.py
@@ -1,16 +1,96 @@
 # @Time 2022/11/2 14:23
 # Author: beijingm
 
-from fastapi import APIRouter
-from pydantic import BaseModel, Field
+import time
+from fastapi import APIRouter, Path, Body
+from fastapi.exceptions import HTTPException
+from fastapi.responses import ORJSONResponse
+from fastapi import exception_handlers
+from pydantic import BaseModel, BaseConfig, Field, EmailStr, PrivateAttr, root_validator, validator
+import orjson
+from typing import Optional
 
-access = APIRouter(prefix="/access")
+from app.module_route import schema
+
+access = APIRouter(prefix="/access", default_response_class=ORJSONResponse)
 
 
-class UserModel(BaseModel):
-    id: int = Field(example=10)
+class OrmBaseModel(BaseModel):
+    class Config:
+        orm_mode = True
+        json_loads = orjson.loads
+        json_dumps = orjson.dumps
+
+    # # 超高端操作，全局验证其。value是每一条UserModelOut数据。
+    # @root_validator
+    # def validate_value(cls, value: dict):
+    #     # {'username': 'beijingm', 'user_id': 100100100, 'custom_id': 100, 'nickname': '大帅', 'avatar': None, 'telephone': '18651815400', 'email': '58149279@qq.com'}
+    #     if value == "":
+    #         return None
+    #     else:
+    #         return value
 
 
-@access.get("/users", )
-def users():
-    return "hello"
+class UserModelOut(OrmBaseModel):
+    username: str = Field(default="lisa", description="用户名", title="USERNAME")
+    user_id: int
+    custom_id: int
+    nickname: str = ""
+    avatar: Optional[str] = Field(default="", description="用户头像", title="USERNAME")
+    telephone: int
+    email: EmailStr
+    role: int
+    status: int
+    address_detail: str = ""
+
+    # validator可以接收当前类已存在的字段，作为校验字段
+    # pre说明在所有验证规则之前验证
+    @validator("avatar", pre=True, always=True)
+    def validate_date(cls, value):
+        if value == "":
+            return None
+        else:
+            return value
+
+
+class UserModelIn(OrmBaseModel):
+    username: str = Field(..., description="用户名", title="USERNAME", example="lisa", repr=False)
+    password: str = Field(..., description="密码", title="PASSWORD", example="*****", repr=False)
+
+    nickname: str = Field(default="小帅", description="用户昵称", title="NICKNAME", repr=False)
+    avatar: str = ""
+    telephone: int
+    email: EmailStr
+
+    province: str = ""
+    city: str = ""
+    county: str = ""
+    address: str = ""
+
+
+@access.get("/users", response_model=list[UserModelOut])
+async def users():
+    return await schema.AsyncUserCURD.select_users()
+
+
+# @access.get("/users", response_model=list[UserModel])
+# def users():
+#     return schema.SyncUserCURD.select_user()
+
+@access.get("/user/{user_id}", response_model=UserModelOut)
+async def user(user_id: int = Path(..., example=100100100)):
+    sql = schema.select(schema.TbUsers).where(schema.TbUsers.user_id == user_id)
+    return await schema.AsyncUserCURD.select_user(sql)
+
+
+@access.post("/user/add", response_model=UserModelOut)
+async def add_user(user: UserModelIn = Body()):
+    exist_user: schema.TbUsers = await schema.AsyncUserCURD.select_user_by_model(user)
+    if exist_user:
+        raise HTTPException(status_code=404, detail="234")
+
+    u = user.dict()
+    u.update({"user_id": int(time.time() * 10000000), "custom_id": int(time.time() * 10000000)})
+    tb_user = schema.TbUsers(**u)
+    _user: schema.TbUsers = await schema.AsyncUserCURD.add_user(tb_user)
+    return _user
--- app/module_route/schema.py
+++ app/module_route/schema.py
@@ -1,18 +1,24 @@
-from sqlalchemy import Column, Integer, String, create_engine, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, create_engine, Boolean, DateTime, BigInteger, SmallInteger
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_scoped_session
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import column_property, Session, sessionmaker, scoped_session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import select, insert, update, delete
-from sqlalchemy.engine.result import ChunkedIteratorResult
-from typing import Union
+from sqlalchemy.engine.result import ChunkedIteratorResult, Result
+from fastapi.exceptions import HTTPException
+from sqlalchemy import or_, and_
 
-# engine = create_engine("mysql+pymysql://root:Root123!@localhost/tms")
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .access import UserModelIn
+
+# engine = create_engine("mysql+pymysql://root:Root123!@localhost/tms", pool=None, pool_size=1, pool_recycle=3600)
 # Base = declarative_base(bind=engine)
-# s = sessionmaker(bind=engine)
+# sync_session = sessionmaker(bind=engine)
 
-async_engine = create_async_engine("mysql+aiomysql://root:Root123!@localhost/tms")
+async_engine = create_async_engine("mysql+aiomysql://root:Root123!@localhost/tms", pool_size=100, pool_recycle=3600)
 Base = declarative_base(bind=async_engine)
 async_session = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
@@ -55,15 +61,15 @@ class TbUsers(Base):
     id = Column("ID", Integer, primary_key=True, nullable=False, autoincrement=True, comment="主键ID")
     username = Column("USERNAME", String(50), nullable=False, unique=True, index=True, comment="用户名")
     password = Column("PASSWORD", String(200), nullable=False, comment="用户密码")
-    user_id = Column("USERID", Integer, nullable=False, unique=True, index=True, comment="用户ID")
-    custom_id = Column("CUSTOM_ID", Integer, nullable=False, unique=True, index=True, comment="客户ID")
+    user_id = Column("USERID", BigInteger, nullable=False, unique=True, index=True, comment="用户ID")
+    custom_id = Column("CUSTOM_ID", BigInteger, nullable=False, unique=True, index=True, comment="客户ID")
 
     nickname = Column("NICKNAME", String(128), comment="用户昵称")
     avatar = Column("AVATAR", String(128), comment="用户头像")
-    telephone = Column("TELEPHONE", String(18), nullable=False, unique=True, comment="手机号")
+    telephone = Column("TELEPHONE", BigInteger, nullable=False, unique=True, comment="手机号")
     email = Column("EMAIL", String(50), nullable=False, unique=True, comment="电子邮件")
-    status = Column("STATUS", Integer, default=0, comment="用户状态")
-    role = Column("ROLE", Integer, default=1, comment="用户角色")
+    status = Column("STATUS", SmallInteger, default=0, comment="用户状态")
+    role = Column("ROLE", SmallInteger, default=1, comment="用户角色")
 
     province = Column("PROVINCE", String(50), comment="省份")
     city = Column("CITY", String(50), comment="城市")
@@ -80,24 +86,68 @@ class TbUsers(Base):
     delete_time = Column("DELETE_TIME", DateTime, comment="删除时间")
 
 
+# class SyncUserCURD:
+#     TbUsers = TbUsers
+#
+#     @staticmethod
+#     def add_user(user):
+#         with sync_session() as session:
+#             session: Session
+#             with session.begin():
+#                 session.add(user)
+#
+#     @staticmethod
+#     def select_user():
+#         with sync_session() as session:
+#             session: Session
+#             sql = select(TbUsers)
+#             result: Result = session.execute(sql)
+#             _users: list[TbUsers] = result.scalars().all()
+#             return _users
+
+
 class AsyncUserCURD:
     TbUsers = TbUsers
+    select = select
+    update = update
 
     @staticmethod
-    async def add_user(user):
+    async def select_user_by_model(user: "UserModelIn") -> TbUsers:
+        async with async_session() as session:
+            sql = select(TbUsers).where(
+                or_(TbUsers.username == user.username,
+                    TbUsers.telephone == user.telephone,
+                    TbUsers.email == user.email)
+            )
+            result: ChunkedIteratorResult = await session.execute(sql)
+            _user: TbUsers = result.scalars().first()
+            return _user
+
+    @staticmethod
+    async def add_user(user: TbUsers):
         async with async_session() as session:
             session: AsyncSession
             async with session.begin():
                 session.add(user)
             await session.commit()
+            return await AsyncUserCURD.select_user_by_model(user)
 
     @staticmethod
-    async def select_user():
+    async def select_users():
         async with async_session() as session:
-            sql = select(TbUsers).where(TbUsers.username == "beijingm")
+            sql = select(TbUsers)
             result: ChunkedIteratorResult = await session.execute(sql)
-            user: TbUsers = result.scalars().first()
-            print(user)
+            _users: list[TbUsers] = result.scalars().all()
+            return _users
+
+    @staticmethod
+    async def select_user(sql):
+        async with async_session() as session:
+            result: ChunkedIteratorResult = await session.execute(sql)
+            _user: TbUsers = result.scalars().first()
+            if not _user:
+                raise HTTPException(status_code=404, detail="资源未找到")
+            return _user
 
     @staticmethod
     async def update_user():
@@ -127,11 +177,14 @@ if __name__ == '__main__':
 
         await async_engine.dispose()
 
+    asyncio.run(create_all())
 
-    user = TbUsers(username="zhangsan", password="root@123", user_id=100100101, custom_id=101, nickname="二狗",
-                   telephone="18651815401", email="68149278@qq.com")
+    user = TbUsers(username="lisa", password="root@123", user_id=100100102, custom_id=102, nickname="三妹",
+                   telephone=18651815402, email="78149278@qq.com")
 
-    asyncio.run(AsyncUserCURD.add_user(user))
-    asyncio.run(AsyncUserCURD.update_user())
-    asyncio.run(AsyncUserCURD.delete_user())
-    asyncio.run(AsyncUserCURD.select_user())
+    # asyncio.run(AsyncUserCURD.add_user(user))
+    # sql = select(TbUsers)
+    # # asyncio.run(AsyncUserCURD.update_user())
+    # # asyncio.run(AsyncUserCURD.delete_user())
+    # a = asyncio.run(AsyncUserCURD.select_users())
+    # print(a)
